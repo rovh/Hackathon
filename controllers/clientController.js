a2e7157c57a652369f412dc6efa636a22fece884
rovh <56371012+rovh@users.noreply.github.com>
2021-12-31 13:19:25 +0300

    c

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -4,8 +4,10 @@ const ApiError = require('../error/ApiError');
 
 class ClientController {
     async create(req, res) {
-        const { name } = req.body
-        const client = await Client.create({ name })
+        const { name, tips_amount } = req.body
+        const date_of_action = new Date()
+        date_of_action = date_of_action.toLocaleDateString()
+        const client = await Client.create({ name, tips_amount, date_of_action })
         return res.json(client)
     }
     async getAll(req, res) {

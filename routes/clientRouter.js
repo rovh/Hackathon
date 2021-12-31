4fe2ab0209400a1c6a9d549209a52fc890ea7ad8
rovh <56371012+rovh@users.noreply.github.com>
2021-12-31 16:18:49 +0300

    c

--- routes/clientRouter.js
+++ routes/clientRouter.js
@@ -4,7 +4,7 @@ const clientController = require('../controllers/clientController')
 const tipsController = require('../controllers/tipsController')
 
 
-router.post('/', clientController.create, tipsController.create)
+router.post('/', clientController.create)
 router.get('/', clientController.getAll)
 router.get('/', clientController.getOne)
 

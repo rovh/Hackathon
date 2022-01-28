419fe8178ad3f9e259b484e5d6d3e6b75f3bf71b
rovh <56371012+rovh@users.noreply.github.com>
2022-01-28 23:15:13 +0300

    c

--- routes/clientRouter.js
+++ routes/clientRouter.js
@@ -2,9 +2,10 @@ const Router = require('express')
 const router = new Router()
 const clientController = require('../controllers/clientController')
 const tipsController = require('../controllers/tipsController')
+const userController = require('../controllers/userController')
 
 
-router.post('/', clientController.create, tipsController.create)
+router.post('/', clientController.create, tipsController.create, userController.create)
 router.get('/', clientController.getAll)
 router.get('/:id', clientController.getOne)
 

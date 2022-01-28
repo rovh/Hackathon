9c559783a9789de52ca485f09f9adda2f25e9a7c
rovh <56371012+rovh@users.noreply.github.com>
2022-01-28 23:28:19 +0300

    c

--- controllers/tipsController.js
+++ controllers/tipsController.js
@@ -3,7 +3,7 @@ const ApiError = require('../error/ApiError');
 const clientController = require('./clientController');
 
 class TipsController {
-    async create(req, res) {
+    async create(req, res, next) {
         const client_id = clientController.client_id
         const name = clientController.client_name
         const tips_amount = clientController.client_tips_amount
@@ -14,9 +14,9 @@ class TipsController {
    
         module.exports.tips_id_export = tip.id 
 
-        return res.json(tip)
+        // return res.json(tip)
 
-        // next()
+        next()
     }
 
     async getAll(req, res) {

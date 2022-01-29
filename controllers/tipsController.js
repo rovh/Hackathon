f6927afd99856cb6a0831e9f35c52830dbe26065
rovh <56371012+rovh@users.noreply.github.com>
2022-01-29 05:01:52 +0300

    c

--- controllers/tipsController.js
+++ controllers/tipsController.js
@@ -4,15 +4,15 @@ const clientController = require('./clientController');
 
 class TipsController {
     async create(req, res, next) {
-        var lastRow = Client[ Client.length - 1 ];
+        // var lastRow = Client[ Client.length - 1 ];
 
-        // const client_id = clientController.client_id
-        // const name = clientController.client_name
-        // const tips_amount = clientController.client_tips_amount
+        const client_id = clientController.client_id
+        const name = clientController.client_name
+        const tips_amount = clientController.client_tips_amount
 
-        const client_id = lastRow.id
-        const name = lastRow.name
-        const tips_amount = lastRow.tips_amount
+        // const client_id = lastRow.id
+        // const name = lastRow.name
+        // const tips_amount = lastRow.tips_amount
 
 
         const date_of_action = new Date().toString();
